7d166250c0679a8dd2afd0f08a954df5ee1600f5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-01-12 12:35:28 +0200

    Updated copyright headers. Happy New Year!

--- src/Exception/CannotOpenFileForRead.php
+++ src/Exception/CannotOpenFileForRead.php
@@ -3,7 +3,7 @@
  * Akeeba Engine
  * The modular PHP5 site backup engine
  *
- * @copyright Copyright (c)2006-2015 Nicholas K. Dionysopoulos
+ * @copyright Copyright (c)2006-2016 Nicholas K. Dionysopoulos
  * @license   GNU GPL version 3 or, at your option, any later version
  * @package   akeebaengine
  */
