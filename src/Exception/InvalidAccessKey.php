19e8d4170f63fcb93d4b10c304c1adc96710578d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-01-01 14:06:40 +0200

    Updated Copyright headers. Happy New Year!

--- src/Exception/InvalidAccessKey.php
+++ src/Exception/InvalidAccessKey.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Akeeba Engine
- * The modular PHP5 site backup engine
+ * The PHP-only site backup engine
  *
- * @copyright Copyright (c)2006-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright Copyright (c)2006-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU GPL version 3 or, at your option, any later version
  * @package   akeebaengine
  */
