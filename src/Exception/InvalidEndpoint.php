96208915514b19e7789ea053c6a109b212864001
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-01-02 09:13:47 +0200

    Update copyright headers. Happy New Year!

--- src/Exception/InvalidEndpoint.php
+++ src/Exception/InvalidEndpoint.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * Akeeba Engine
- * The PHP-only site backup engine
  *
- * @copyright Copyright (c)2006-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU GPL version 3 or, at your option, any later version
  * @package   akeebaengine
+ * @copyright Copyright (c)2006-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
  */
 
 namespace Akeeba\Engine\Postproc\Connector\S3v4\Exception;
