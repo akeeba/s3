1ec8495685e3d3080840c954d383f19dec9309b7
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-02 15:31:06 +0200

    Updating copyright. Happy New Year!

--- minitest/Test/Multipart.php
+++ minitest/Test/Multipart.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Akeeba Engine
+ *
+ * @package   akeebaengine
+ * @copyright Copyright (c)2006-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
 
 namespace Akeeba\MiniTest\Test;
 
