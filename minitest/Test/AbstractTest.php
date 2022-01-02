5dbc8d13d1aebbcbf23ee352a096c3e429de3a03
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-01-02 11:18:46 +0200

    Copyright update. Happy New Year!

--- minitest/Test/AbstractTest.php
+++ minitest/Test/AbstractTest.php
@@ -3,7 +3,7 @@
  * Akeeba Engine
  *
  * @package   akeebaengine
- * @copyright Copyright (c)2006-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright Copyright (c)2006-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU General Public License version 3, or later
  */
 
